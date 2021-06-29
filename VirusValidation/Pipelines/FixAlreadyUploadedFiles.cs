49d4c532c658c3d5167b34e665cbb43e739d220b
Jan Bluemink <jbluemink@mirabeau.nl>
2021-06-29 13:53:07 +0200

    initieel

---
+++ VirusValidation/Pipelines/FixAlreadyUploadedFiles.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceForms.Data;
+using Sitecore.ExperienceForms.Data.Entities;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Mvc;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+using Sitecore.ExperienceForms.Mvc.Pipelines.ExecuteSubmit;
+using Sitecore.Mvc.Pipelines;
+
+namespace VirusValidation.Pipelines
+{
+    public class FixAlreadyUploadedFiles : MvcPipelineProcessor<ExecuteSubmitActionsEventArgs>
+    {
+        private readonly IFormRenderingContext _formRenderingContext;
+        private readonly IFileStorageProvider _fileStorageProvider;
+
+        public FixAlreadyUploadedFiles(
+            IFormRenderingContext formRenderingContext,
+            IFileStorageProvider fileStorageProvider)
+        {
+            Assert.ArgumentNotNull((object)formRenderingContext, nameof(formRenderingContext));
+            Assert.ArgumentNotNull((object)fileStorageProvider, nameof(fileStorageProvider));
+            this._formRenderingContext = formRenderingContext;
+            this._fileStorageProvider = fileStorageProvider;
+        }
+
+        public override void Process(ExecuteSubmitActionsEventArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, nameof(args));
+            if (args.FormSubmitContext?.Fields == null)
+                return;
+            IList<IViewModel> fields = args.FormSubmitContext?.Fields;
+            if (fields == null)
+                return;
+            foreach (IViewModel viewModel in (IEnumerable<IViewModel>)fields)
+            {
+                if (viewModel is FileUploadViewModel fileUploadViewModel)
+                {
+                    if (fileUploadViewModel.Files != null)
+                    {
+                        List<StoredFileInfo> storedFileInfoList = GetNoVirusFileUpload(fileUploadViewModel.Files);
+                        if (storedFileInfoList != null)
+                        {
+                            fileUploadViewModel.Value = storedFileInfoList;
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+
+        private List<StoredFileInfo> GetNoVirusFileUpload(List<HttpPostedFileBase> fileList)
+        {
+            if (_formRenderingContext.PostedFields != null)
+            {
+                foreach (var postedFieldField in _formRenderingContext.PostedFields)
+                {
+                    if (postedFieldField.Name == "Face")
+                    {
+                        var fileField = (FileUploadViewModel)postedFieldField;
+                        if (fileField.Files == fileList)
+                        {
+                            return fileField.Value;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ VirusValidation/VirusValidation.cs
@@ -0,0 +1,145 @@
+﻿using Sitecore.ExperienceForms.Data;
+using System;
+using System.Collections.Generic;
+using System.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.ExperienceForms.Data.Entities;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Mvc;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+using Sitecore.ExperienceForms.Mvc.Models.Validation;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using Cloudmersive.APIClient.NET.VirusScan.Client;
+using Cloudmersive.APIClient.NET.VirusScan.Api;
+using Cloudmersive.APIClient.NET.VirusScan.Model;
+
+namespace VirusValidation
+{
+    public class VirusValidation : ValidationElement<string>
+    {
+        private IFormRenderingContext _formRenderingContext;
+        private IFileStorageProvider _fileStorageProvider;
+        private ValidationDataModel _validationItem;
+
+        public VirusValidation(ValidationDataModel validationItem) : base(validationItem)
+        {
+            this._validationItem = validationItem;
+            this._fileStorageProvider = ServiceLocator.ServiceProvider.GetService<IFileStorageProvider>();
+            this._formRenderingContext = ServiceLocator.ServiceProvider.GetService<IFormRenderingContext>(); ;
+        }
+
+        public override IEnumerable<ModelClientValidationRule> ClientValidationRules
+        {
+            get
+            {
+                var clientValidationRule = new ModelClientValidationRule
+                {
+                    ErrorMessage = FormatMessage(Title),
+                    ValidationType = "regex"
+                };
+
+                yield return clientValidationRule;
+            }
+        }
+
+        public string Title { get; set; }
+
+        public override ValidationResult Validate(object value)
+        {
+            if (value != null)
+            {
+                List<HttpPostedFileBase> httpPostedFileBaseList = value as List<HttpPostedFileBase>;
+                if (httpPostedFileBaseList != null)
+                {
+
+                    List<StoredFileInfo> storedFiledList = new List<StoredFileInfo>();
+
+                    foreach (HttpPostedFileBase httpPostedFileBase in httpPostedFileBaseList)
+                    {
+                        if (httpPostedFileBase == null)
+                        {
+                            return new ValidationResult(FormatMessage(Title));
+                        }
+
+                        var apikey = Sitecore.Configuration.Settings.GetSetting("CloudmersiveApiKey");
+                        if (string.IsNullOrEmpty(apikey))
+                        {
+                            Logger.Warn("CloudmersiveApiKey is empty");
+                        }
+                        Configuration.Default.AddApiKey("Apikey", apikey);
+
+
+                        var file = ReadAllBytes(httpPostedFileBase.InputStream);
+
+                        using (MemoryStream stream = new MemoryStream(file))
+                        {
+                            var apiInstance = new ScanApi();
+
+                            try
+                            {
+                                // Scan a file for viruses
+                                VirusScanResult result = apiInstance.ScanFile((Stream)stream);
+                                if (result.FoundViruses != null && result.FoundViruses.Count > 0)
+                                {
+                                    return new ValidationResult(FormatMessage(Title));
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Logger.Warn("Form Virusscan Cloudmersive Exception ", e.Message);
+                                return new ValidationResult("Error with Virus scanner");
+                            }
+
+                            stream.Position = 0;
+                            var fileId = this._fileStorageProvider.StoreFile(stream, httpPostedFileBase.FileName);
+                            storedFiledList.Add(new StoredFileInfo()
+                            {
+                                FileId = fileId,
+                                FileName = httpPostedFileBase.FileName
+                            });
+                        }
+
+                    }
+
+                    // Add the stored image to the postedFileList
+                    List<IViewModel> postedFieldList = new List<IViewModel>();
+                    postedFieldList.Add(new FileUploadViewModel()
+                    {
+                        AllowSave = false,
+                        Name = "Face",
+                        ItemId = Guid.NewGuid().ToString(),
+                        Value = storedFiledList,
+                        Files = httpPostedFileBaseList
+                    });
+                    _formRenderingContext.StorePostedFields(postedFieldList);
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        public byte[] ReadAllBytes(Stream stream)
+        {
+            using (var ms = new MemoryStream())
+            {
+                stream.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+
+        public override void Initialize(object validationModel)
+        {
+            base.Initialize(validationModel);
+
+            var obj = validationModel as StringInputViewModel;
+            if (obj != null)
+            {
+                Title = obj.Title;
+            }
+        }
+
+
+    }
+}
